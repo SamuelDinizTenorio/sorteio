6c6b6a480c4226bfb923dc1eb5237fedfdf3257b
Samuel Diniz Tenório <samueldinizcti@outlook.com>
2025-05-05 16:47:20 -0300

    Add launch configuration and enhance UI with additional circles and animations

--- script.js
+++ script.js
@@ -4,14 +4,44 @@ const drawBtn = document.getElementById('drawBtn');
 const winner = document.getElementById('winner');
 
 // Array de participantes
-const participants = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
+const participants = ['Kelvia', 'Carla', 'Josinaldo', 'Diane', 'Maria Edna', 'Terezinha', 'Silvia', 'Maria José Diniz', 'Aparecida', 'Poliana', 'Jeane Diniz', 'Hina', 'Kamila', 'Jamila'];
+
+// Função para animar os círculos
+function animateCircles() {
+    const circles = document.querySelectorAll('.circle');
+    circles.forEach((circle, index) => {
+        circle.style.animation = `floatUp 6s ease-in-out ${index * 0.7}s forwards infinite`; // Aumenta o delay entre os círculos
+    });
+}
 
 // Adicionando um evento de clique ao botão de sorteio
 drawBtn.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * participants.length); // Gera um índice aleatório
-    const selectedParticipant = participants[randomIndex]; // Seleciona um participante aleatório
-    winner.textContent = `Vencedor: ${selectedParticipant}`; // Exibe o vencedor
-    winner.style.display = 'block'; // o vencedor aparece após o sorteio
-    drawBtn.disabled = true; // Disativa o botão após o sorteio
-    drawBtn.textContent = 'Sorteio realizado!'; // Muda o texto do botão
+    const countdown = [3, 2, 1]; // Array com os números da contagem regressiva
+
+    // Loop para criar a contagem regressiva
+    for (let i = 0; i < countdown.length; i++) {
+        setTimeout(() => {
+            drawBtn.textContent = countdown[i]; // Atualiza o texto do botão com o número da contagem
+        }, (i + 1) * 1000); // Define o tempo de espera para cada número
+    }
+
+    // Após a contagem regressiva, atualiza o texto do botão para "Sorteio em andamento..."
+    setTimeout(() => {
+        animateCircles(); // Chama a função para animar os círculos
+    }, (1 * 1000)); // Espera 1 segundo antes de iniciar a animação
+
+    // Após a contagem regressiva, realiza o sorteio
+    setTimeout(() => {
+        const randomIndex = Math.floor(Math.random() * participants.length); // Gera um índice aleatório
+        const selectedParticipant = participants[randomIndex]; // Seleciona um participante aleatório
+        winner.textContent = `Vencedor(a): ${selectedParticipant}`; // Exibe o vencedor
+        winner.style.display = 'block'; // Mostra o vencedor
+        drawBtn.disabled = true; // Desativa o botão após o sorteio
+        drawBtn.textContent = 'Sorteio realizado!'; // Atualiza o texto do botão
+        
+    }, (countdown.length + 1) * 1000); // Espera o tempo da contagem regressiva antes de realizar o sorteio
+
+    setTimeout(() => {
+        
+    }, (countdown.length + 1) * 1500);
 });
